9417deb2cda5476058036eedb02dc12642500062
Yangyukai0129 <beir6108@gmail.com>
2025-11-02 21:15:39 +0800

    Add config temporarily

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-OPENROUTER_API_KEY = "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"
+OPENROUTER_API_KEY = "xxxx"
 MODEL = "mistralai/voxtral-small-24b-2507"
 
 DATA_DIR = "./data/post"
